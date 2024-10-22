4ec469ff4b88f11181433007cc70ecf684149d22
Himanjal saha <84189618+himanjalsaha@users.noreply.github.com>
2024-10-22 21:47:22 +0530

    Update Localstorage.ts

--- src/hooks/Localstorage.ts
+++ src/hooks/Localstorage.ts
@@ -2,8 +2,10 @@
 import { useState } from 'react';
 
 const useLocalStorage = (key: string, initialValue: any) => {
+  // // Get the item from local storage
   const [storedValue, setStoredValue] = useState(() => {
     try {
+      // Return the parsed item or the initial value
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
@@ -14,6 +16,7 @@ const useLocalStorage = (key: string, initialValue: any) => {
 
   const setValue = (value: any) => {
     try {
+      // If value is a function, call it with the current stored value
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
